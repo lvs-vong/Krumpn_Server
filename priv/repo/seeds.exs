9482441f5f8fde656d57926ba39236bbdd6bd42e
lvs-vong <evong@nevada.unr.edu>
2022-07-11 22:34:02 -0700

    add a dummy recipe in the seeder

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias KrumpnServer.Repo
+
+%KrumpnServer.Recipes.Recipe{name: "Chicken Nuggets",
+                            description: "Chicken nuggies for breakfast, I guess.",
+                            directions: ["1. step one goes here", "2. step 2 here", "3. lastly, step 3 heere"],
+                            prep_time: 5,
+                            num_servings: 1,
+                            num_calories: 500.0
+                          } |> Repo.insert()
